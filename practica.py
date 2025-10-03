a543dc37441d557d62ddacd6ba4eb5cd45ab256e
CamiloWiesner <gironwiesnerjuancamilo11@outlook.com>
2025-10-03 19:35:12 +0200

    Una pequeña calculadora básica

---
+++ practica.py
@@ -0,0 +1,7 @@
+numero = int(input("Introduce un número: "))
+numero2 = int(input("Introduce un segundo número: "))
+suma = numero + numero2 
+resta = numero - numero2
+division = numero / numero2 
+multiplicacion = numero*numero2
+print(f"{suma}\n{resta}\n{division}\n{multiplicacion}")
